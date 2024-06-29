f72904448877b1d3dabb4d87c5157b1cd9f65b54
Rey <reudysfernando@gmail.com>
2024-06-29 19:13:48 -0400

    Bot v3

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using System.Threading;
 using OpenQA.Selenium.Interactions;
 using System.Drawing;
 
-
 class Program
 {
     static void Main(string[] args)
@@ -26,107 +25,88 @@ class Program
                 Thread.Sleep(2000);
 
                 // ID's de los radio button
-                String[] IdR1 = ["i5","i8","i11","i14","i17","i20"]; 
+                String[] IdR1 = {"i5","i8","i11","i14","i17","i20"}; 
 
                 // Seleccionamos el radio button por su ID
-                var radioButton = driver.FindElement(By.Id(IdR1[NumRandom6()]));
+                var radioButton = driver.FindElement(By.Id(IdR1[NumRandom6(IdR1.Length)]));
 
                 // Simulamos hacer clic en el radio button
                 radioButton.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR2 = ["i27","i30","i33","i36"];
-                var radioButton2 = driver.FindElement(By.Id(IdR2[NumRandom4()]));
+                String[] IdR2 = {"i27","i30","i33","i36"};
+                var radioButton2 = driver.FindElement(By.Id(IdR2[NumRandom4(IdR2.Length)]));
                 radioButton2.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR3 = ["i43","i46","i49","i52","i55","i58"];
-                var radioButton3 = driver.FindElement(By.Id(IdR3[NumRandom6()]));
+                String[] IdR3 = {"i43","i46","i49","i52","i55","i58"};
+                var radioButton3 = driver.FindElement(By.Id(IdR3[NumRandom6(IdR3.Length)]));
                 radioButton3.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR4 = ["i65","i68","i71","i74","i77"];
-                var radioButton4 = driver.FindElement(By.Id(IdR4[NumRandom5()]));
+                String[] IdR4 = {"i65","i68","i71","i74","i77"};
+                var radioButton4 = driver.FindElement(By.Id(IdR4[NumRandom5(IdR4.Length)]));
                 radioButton4.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR5 = ["i84","i87","i90","i93","i96"];
-                var radioButton5 = driver.FindElement(By.Id(IdR5[NumRandom5()]));
+                String[] IdR5 = {"i84","i87","i90","i93","i96"};
+                var radioButton5 = driver.FindElement(By.Id(IdR5[NumRandom5(IdR5.Length)]));
                 radioButton5.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR6 = ["i103","i106","i109","i112","i115"];
-                var radioButton6 = driver.FindElement(By.Id(IdR6[NumRandom5()]));
+                String[] IdR6 = {"i103","i106","i109","i112","i115"};
+                var radioButton6 = driver.FindElement(By.Id(IdR6[NumRandom5(IdR6.Length)]));
                 radioButton6.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR7 = ["i122","i125","i128","i131","i134"];
-                var radioButton7 = driver.FindElement(By.Id(IdR7[NumRandom5()]));
+                String[] IdR7 = {"i122","i125","i128","i131","i134"};
+                var radioButton7 = driver.FindElement(By.Id(IdR7[NumRandom5(IdR7.Length)]));
                 radioButton7.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR8 = ["i141","i144","i147","i150","i153"];
-                var radioButton8 = driver.FindElement(By.Id(IdR8[NumRandom5()]));
+                String[] IdR8 = {"i141","i144","i147","i150","i153"};
+                var radioButton8 = driver.FindElement(By.Id(IdR8[NumRandom5(IdR8.Length)]));
                 radioButton8.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR9 = ["i160","i163","i166","i169","i172"];
-                var radioButton9 = driver.FindElement(By.Id(IdR9[NumRandom5()]));
+                String[] IdR9 = {"i160","i163","i166","i169","i172"};
+                var radioButton9 = driver.FindElement(By.Id(IdR9[NumRandom5(IdR9.Length)]));
                 radioButton9.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR10 = ["i179","i182","i185","i188","i191"];
-                var radioButton10 = driver.FindElement(By.Id(IdR10[NumRandom5()]));
+                String[] IdR10 = {"i179","i182","i185","i188","i191"};
+                var radioButton10 = driver.FindElement(By.Id(IdR10[NumRandom5(IdR10.Length)]));
                 radioButton10.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR11 = ["i198","i201","i204","i207","i210"];
-                var radioButton11 = driver.FindElement(By.Id(IdR11[NumRandom5()]));
+                String[] IdR11 = {"i198","i201","i204","i207","i210"};
+                var radioButton11 = driver.FindElement(By.Id(IdR11[NumRandom5(IdR11.Length)]));
                 radioButton11.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR12 = ["i217","i220","i223","i226","i229"];
-                var radioButton12 = driver.FindElement(By.Id(IdR12[NumRandom5()]));
+                String[] IdR12 = {"i217","i220","i223","i226","i229"};
+                var radioButton12 = driver.FindElement(By.Id(IdR12[NumRandom5(IdR12.Length)]));
                 radioButton12.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR13 = ["i236","i239","i242","i245","i248"];
-                var radioButton13 = driver.FindElement(By.Id(IdR13[NumRandom5()]));
+                String[] IdR13 = {"i236","i239","i242","i245","i248"};
+                var radioButton13 = driver.FindElement(By.Id(IdR13[NumRandom5(IdR13.Length)]));
                 radioButton13.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR14 = ["i255","i258","i261","i264","i267"];
-                var radioButton14 = driver.FindElement(By.Id(IdR14[NumRandom5()]));
+                String[] IdR14 = {"i255","i258","i261","i264","i267"};
+                var radioButton14 = driver.FindElement(By.Id(IdR14[NumRandom5(IdR14.Length)]));
                 radioButton14.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR15 = ["i274","i277","i280","i283","i286"];
-                var radioButton15 = driver.FindElement(By.Id(IdR15[NumRandom5()]));
+                String[] IdR15 = {"i274","i277","i280","i283","i286"};
+                var radioButton15 = driver.FindElement(By.Id(IdR15[NumRandom5(IdR15.Length)]));
                 radioButton15.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR16 = ["i293","i296","i299","i302","i305"];
-                var radioButton16 = driver.FindElement(By.Id(IdR16[NumRandom5()]));
+                String[] IdR16 = {"i293","i296","i299","i302","i305"};
+                var radioButton16 = driver.FindElement(By.Id(IdR16[NumRandom5(IdR16.Length)]));
                 radioButton16.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR17 = ["i312","i315","i318","i321","i324"];
-                var radioButton17 = driver.FindElement(By.Id(IdR17[NumRandom5()]));
+                String[] IdR17 = {"i312","i315","i318","i321","i324"};
+                var radioButton17 = driver.FindElement(By.Id(IdR17[NumRandom5(IdR17.Length)]));
                 radioButton17.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR18 = ["i331", "i334","i337","i340","i343"];
-                var radioButton18 = driver.FindElement(By.Id(IdR18[NumRandom5()]));
+                String[] IdR18 = {"i331","i334","i337","i340","i343"};
+                var radioButton18 = driver.FindElement(By.Id(IdR18[NumRandom5(IdR18.Length)]));
                 radioButton18.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR19 = ["i350","i353","i356","i359","i362"];
-                var radioButton19 = driver.FindElement(By.Id(IdR19[NumRandom5()]));
+                String[] IdR19 = {"i350","i353","i356","i359","i362"};
+                var radioButton19 = driver.FindElement(By.Id(IdR19[NumRandom5(IdR19.Length)]));
                 radioButton19.Click();
-                //Thread.Sleep(1000);
                 //-------------------------
-                String[] IdR20 = ["i369","i372","i375","i378","i381"];
-                var radioButton20 = driver.FindElement(By.Id(IdR20[NumRandom5()]));
+                String[] IdR20 = {"i369","i372","i375","i378","i381"};
+                var radioButton20 = driver.FindElement(By.Id(IdR20[NumRandom5(IdR20.Length)]));
                 radioButton20.Click();
                 Thread.Sleep(1000);
                 //-------------------------
@@ -186,24 +166,33 @@ class Program
         }
     }
 
-    static int NumRandom4()
+    static int NumRandom4(int numRespuestas)
     {
-        Random rdc = new Random();
-        int num = rdc.Next(0, 4);
-        return num;
+        return GetRandomNormalIndex(numRespuestas);
     }
 
-    static int NumRandom5()
+    static int NumRandom5(int numRespuestas)
     {
-        Random rdc = new Random();
-        int num = rdc.Next(0, 5);
-        return num;
+        return GetRandomNormalIndex(numRespuestas);
     }
 
-    static int NumRandom6()
+    static int NumRandom6(int numRespuestas)
     {
-        Random rdc = new Random();
-        int num = rdc.Next(0, 6);
-        return num;
+        return GetRandomNormalIndex(numRespuestas);
+    }
+
+    static int GetRandomNormalIndex(int numRespuestas)
+    {
+        Random random = new Random();
+        double mean = random.Next(0, numRespuestas); // Media aleatoria entre las opciones
+        double stdDev = numRespuestas / 3.0; // Desviación estándar ajustada al rango de las opciones
+
+        double u1 = 1.0 - random.NextDouble(); // uniform(0,1] random doubles
+        double u2 = 1.0 - random.NextDouble();
+        double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); // random normal(0,1)
+        double randNormal = mean + stdDev * randStdNormal; // random normal(mean, stdDev^2)
+
+        int response = (int)Math.Round(Math.Max(0, Math.Min(numRespuestas - 1, randNormal)));
+        return response;
     }
 }
--- obj/Debug/net8.0/Bot1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bot1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bot1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f83c4769301cb19cc0d22e4e647301fcaf8a9b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bot1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bot1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
